0be93afb4c89da9e5195c8cbb62622ac52c20863
Andrew Johnson <ajwanderer@gmail.com>
2025-04-18 20:08:13 -0600

    Added black screen

--- main.py
+++ main.py
@@ -2,9 +2,27 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
+    pygame.init()
+    # Set up the game window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+        # Fill the screen with black
+        screen.fill((0, 0, 0))
+
+        # Update the display
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
