b632884ce12cce47bc6285d63b1131cb127806c2
Andrew Johnson <ajwanderer@gmail.com>
2025-04-24 15:36:09 -0600

    added player movement functionality with forward and backward controls

--- player.py
+++ player.py
@@ -38,4 +38,15 @@ class Player(CircleShape):
         if keys[pygame.K_d]:
             # Rotate right
             self.rotate(-dt)
-           
+
+        if keys[pygame.K_w]:
+            # Move forward
+            self.move(dt)
+            
+        if keys[pygame.K_s]:
+            # Move backward
+            self.move(-dt)
+        
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
