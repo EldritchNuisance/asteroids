87a0edc0f6459233eab1cb5d388ca95c7c3f6a0e
Andrew Johnson <ajwanderer@gmail.com>
2025-05-06 18:10:49 -0600

    feat: implement asteroid splitting mechanism upon collision with shots

--- asteroid.py
+++ asteroid.py
@@ -14,3 +14,18 @@ class Asteroid(CircleShape):
     def update(self, dt):
         # Add velocity * dt to the position
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        # Split the asteroid into smaller ones
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        import random
+        angle = random.uniform(20, 50)
+        velocity1 = self.velocity.rotate(angle)
+        velocity2 = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid1.velocity = velocity1 * 1.2
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2.velocity = velocity2 * 1.2
--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def main():
             for shot in shots:
                 if shot.collide(asteroid):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         # Fill the screen with black
         screen.fill("black")
